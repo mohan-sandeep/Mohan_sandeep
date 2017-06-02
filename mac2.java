24d06cff599c50fa6e2c7443d31daafa8054a953
bilakantiviharika <viharika.bilakanti@spoors.in>
2017-06-02 11:23:18 +0530

    hii

---
+++ mac2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class mac{
+public static void main(String[] args){
+System.out.print("Hello world");
+System.out.print("Hello worl22d");
+System.out.print("Hellbjnjm world");
+
+System.out.print("Hello world again");
+}
+}
