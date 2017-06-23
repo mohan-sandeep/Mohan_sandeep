c92749f42e2c7992ca06f740c996baa89c93e648
mohan-sandeep <sandeep.yeluri@spoors.in>
2017-06-23 15:29:19 +0530

    donee

--- mack.java
+++ mack.java
@@ -3,6 +3,8 @@ class mac{
 public static void main(String[] args){
 System.out.print("Hello world");
 System.out.print("Hello worl22d");
+System.out.print("Hello worl2d");
+System.out.print("Hello worl2");
 System.out.print("Hellbjnjm world");
 snfansfsafjsanjdlnwq
 System.out.print("Hello world again");
