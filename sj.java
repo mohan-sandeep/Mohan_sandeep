0ede995ed647b8515840b7f014f934db4a936c9e
mohan-sandeep <sandeep.yeluri@spoors.in>
2017-06-29 12:17:39 +0530

    sj did

---
+++ sj.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sj
+{
+public static void main(String[] args){
+int arr[]=new int[10];
+System.out.println(arr.length);
+int j=10;
+while(--j!=0){
+if(j<5)
+arr[j]=0;}
+System.out.println(arr.length);
+}
+}
